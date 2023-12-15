8e5051c915f645d53d8eebeea681d56afb297e9a
Veener <vinhernandez789@gmail.com>
2023-12-15 10:07:26 -0500

    Clean Server print and added server logging

--- VSMP_ServerV4.0.py
+++ VSMP_ServerV4.0.py
@@ -2,6 +2,7 @@
 import threading
 import socket
 import time
+import datetime
 
 
 usernameList={
@@ -25,23 +26,23 @@ def __init__():
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind((host, port))
         s.listen()
-        print(f"Listening on {host}: {port}")
+        logprint(f"Listening on {host}: {port}")
         
         while True:
             conn, addr=s.accept()
             clientList.append(conn)
             
-            print(f"got client from{addr[0]}: {addr[1]}")
+            logprint(f"Client from {addr[0]}: {addr[1]} has connected")
             thread=threading.Thread(target=handle_client, args=(conn, addr))
             thread.start()
     except KeyboardInterrupt:
-        print("Manual Break")
+        logprint("Manual Break")
         serverRescue()
     except Exception as e:
-        print(f"Error: {e}")
+        logprint(f"Error: {e}")
     finally:
         s.close()
-        print("Server closed")
+        logprint("Server closed")
 
 
 def handle_client(conn, addr):
@@ -50,58 +51,62 @@ def handle_client(conn, addr):
             data = conn.recv(4096)
             if data:
                 dataL = data.split(b"\0")
-                print(f"Data:{data}, dataL:{dataL}")
+                #print(f"Data:{data}, dataL:{dataL}")
                 username=str(dataL[0].decode("utf-8"))
                 #print(username)
                 #print(f"USernma fr:{dataL[2]}")
+                #print(f"Data: {dataL}")
                 dataL.pop()
                 if username=="SERVER":
                     serverMessageHandler(dataL, conn)
                 else:    
-                    print("Bcast")
+                    #print("Bcast")
                     broadcast(dataL, username)
             else:
                 pass
 
             
     except KeyboardInterrupt:
-        print("KeyStop")
+        logprint("KeyStop")
         serverRescue()
     except Exception as e:
-        print(f"Handler Error: {e}")
+        logprint(f"Handler Error: {e}")
     finally:
         closeClient(conn)
-        print(f"Connection to client ({addr[0]}: {addr[1]}) closed")
+        logprint(f"Connection to client ({addr[0]}: {addr[1]}) closed\n")
 
 def broadcast(messages,  username):
     if not messages or not clientList:
-        print("NAHT")
+        logprint("NAHT")
         return
     """print(str(username)[2:-1])
     usernameCol=str(username)[2:-1] + ": "
     usernameCol
     messages.pop(0)"""
-    print(f"Messages: {messages}")
+    logprint(f"Message: {messages}")
     messageList=b""
     for message in messages:
         if not message:
-            print("empty byte. Breaking")
+            logprint("empty byte. Breaking")
             break
         messageList+=message
         messageList += b"\0"
-        print(f"#{username}: {message}")
-    print("created ML")
+        #print(f"#{username}: {message}")
+    #print("created ML")
     try:
-        for client in clientList:    
+        x=0
+        for client in clientList: 
+            x+=1   
             client.send(bytes(messageList))
             time.sleep(.001)
-            print(f"sent to {client}")
-        print(f"ML={username}: {messageList}")
+            #print(f"sent to {client}")
+        #print(f"ML={username}: {messageList}")
+        #logprint(f"Message sent to {x}/{len(usernameList)-3} users\n")
     except KeyboardInterrupt:
-        print("Manual Break")
+        logprint("Manual Break")
         serverRescue()
     finally:
-        print("done sending")
+       logprint(f"Message sent to {x}/{len(usernameList)-3} users\n")
  
 def serverInput():
     while True:
@@ -115,62 +120,62 @@ def serverInput():
             message=input("Message: ")
             serverBroadcast(message)
         elif inp=="ul":
-            print(usernameList.keys())
+            logprint(usernameList.keys())
         else:
-            print("invalid command")
+            logprint("invalid command")
             
 def serverBroadcast(message):
     try:
         for client in clientList:    
             client.send(bytes(f"#SERVER: {message}".encode("utf-8")))
-            print(f"SB to {client}")
+            logprint(f"SB to {client}")
     except KeyboardInterrupt:
-        print("Manual Break")
+        logprint("Manual Break")
         serverRescue()
     finally:
-        print("done SB")
+        logprint("done SB\n")
         
 def serverRescue():
     x=0
     for client in clientList: 
         closeClient(client)
         x=+1
-    print(f"Server Rescued. All({x}) connections severed")
+    logprint(f"Server Rescued. All({x}) connections severed\n")
     
 def closeClient(client):
-    print(f"client at {client} Will be closed")
+    logprint(f"client at {client} Will be closed")
     client.close()
     if client in clientList:
         clientList.remove(client)
     for key, value in usernameList.items():
         if value == client:
             del usernameList[key]
-            print(f"removed {key} from usernameList")
+            logprint(f"{key} has left the chat")
             break
     
   
 def kickClient(username):
     conn=usernameList[username]
     conn.send("*You have been kicked!".encode("utf-8"))
-    print("!You have been kicked!")   
+    logprint(f"!{username} has been kicked!")   
     closeClient(conn) 
   
    
 def checkUsername(username, conn):
     if username in usernameList.keys():
         conn.send("*Username Taken, Try a New One!".encode("utf-8"))
-        print(bytes("!Username Taken, Try a New One!".encode("utf-8")))
-        print(usernameList.keys())
+        logprint(bytes("!Username Taken, Try a New One!".encode("utf-8")))
+        logprint(usernameList.keys())
         closeClient(conn)
     else:
         usernameList[username]=conn
-        print(f"Adding username :{username}")
-        print(usernameList)
+        logprint(f"Adding username: {username}\n")
+        #print(usernameList)
 
 def keyWarn(username):
     client=usernameList[username]
     client.send(b"!A conencted user has a non-compatable key and did not recieve your message!")
-    print(f"Key Warned {username}")
+    logprint(f"Key Warned {username}")
 
 def serverMessageHandler(dataL, conn):
     type=dataL[1].decode("utf-8")
@@ -184,8 +189,19 @@ def serverMessageHandler(dataL, conn):
     elif type=="KeyWarn":
         keyWarn(message)
     else:
-        print("Server Message Not Handled")
+        logprint("Server Message Not Handled")
+        
+def logprint(message):
+    now = datetime.datetime.now()
+    t=now.strftime("%H:%M:%S")
+    cdate=datetime.date.today()
+    
+    fdate = cdate.strftime("%m-%d-%Y")
 
+    print(f"{t} | {message}")
+    with open(f'ServerLogs/{fdate}.txt', 'a') as file:
+        file.write(f"{t} | {message}\n")
+    
 if __name__=="__main__":
     __init__()
 
